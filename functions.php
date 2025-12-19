205c129ac55b7ddd20584a37b7cdf25424762652
Ahamed Asif <138349258+ahamedasif07@users.noreply.github.com>
2025-12-19 20:06:02 +0600

    done

--- functions.php
+++ functions.php
@@ -42,3 +42,43 @@ function ak_theme_assets() {
 }
 
 add_action('wp_enqueue_scripts', 'ak_theme_assets');
+
+
+
+// Theme Customizer function
+function ak_customize_banner($wp_customize) {
+
+    /* =========================
+       1️⃣ SECTION ADD
+       ========================= */
+    $wp_customize->add_section('home_banner_section', array(
+        'title'       => __('Home Banner ', 'asif-domain'),
+        'description' => __('This is home banner image section', 'asif-domain'),
+    ));
+
+
+    /* =========================
+       2️⃣ SETTING ADD
+       ========================= */
+    $wp_customize->add_setting('home_banner_image', array(
+        'default' => get_template_directory_uri() . '/images/banner.png',
+    ));
+
+
+    /* =========================
+       3️⃣ CONTROL ADD (IMAGE UPLOAD)
+       ========================= */
+    $wp_customize->add_control(
+        new WP_Customize_Image_Control(
+            $wp_customize,
+            'home_banner_image',
+            array(
+                'label'    => __('Upload Home Banner Image', 'asif-domain'),
+                'section'  => 'home_banner_section',
+                'settings' => 'home_banner_image',
+            )
+        )
+    );
+}
+
+add_action('customize_register', 'ak_customize_banner');
--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <main>
     <!-- Full Screen Banner -->
     <section class="w-7xl mx-auto bg-gray-200">
-        <img class='object-fill mt-[30px]' src="<?php echo get_template_directory_uri(); ?>/images/banner.png"
-            alt="My Banner">
+        <img class='object-fill mt-[30px]' src="<?php echo get_theme_mod( 'home_banner_image' ) ?>" alt="My Banner">
     </section>
 
     <!-- Content Section -->
